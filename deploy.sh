908f3236df1d531397f174737b7d934acd204f94
Mark Old <dlom234@gmail.com>
2016-02-09 22:21:19 -0800

    improve deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env zsh
 
-echo "mounting SD card..."
-mount /dev/mmcblk0p1 sd
-echo "making sure kernel.img is up to date..."
-make kernel.img
-echo "copying kernel.img over..."
-cp kernel.img sd/
-echo "unmount SD card..."
-umount sd
-echo "syncing data..."
-sync
+if [[ $UID == 0 || $EUID == 0 ]]; then
+    echo "mounting SD card..."
+    mount /dev/mmcblk0p1 sd
+    echo "making sure kernel.img is up to date..."
+    make kernel.img
+    echo "copying kernel.img over..."
+    cp kernel.img sd/
+    echo "unmount SD card..."
+    umount sd
+    echo "syncing data..."
+    sync
+else
+    echo "run as root"
+fi
